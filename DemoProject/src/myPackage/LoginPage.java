06dce2c6c2a89f4702bb4016ca67e1d5f5b8f3a1
abha9514 <abhatripathi12345@gmail.com>
2023-02-06 18:56:37 +0530

    changed

--- DemoProject/src/myPackage/LoginPage.java
+++ DemoProject/src/myPackage/LoginPage.java
@@ -4,5 +4,6 @@ public class LoginPage {
 public static void main(String[] args) {
 	System.out.println("task cmpleted");
 	System.out.println("new feature");
+	System.out.println("i have changed the task");
 }
 }
